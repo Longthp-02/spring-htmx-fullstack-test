991b6862eb5174099a7c809276b4e9957d0653d4
Long Pham <longthp02@gmail.com>
2026-02-11 12:21:00 +0700

    chore: initial Spring Boot Kotlin project setup

---
+++ src/main/kotlin/com/longpham/springhtmxfullstacktest/SpringHtmxFullstackTestApplication.kt
@@ -0,0 +1,11 @@
+package com.longpham.springhtmxfullstacktest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringHtmxFullstackTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringHtmxFullstackTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/longpham/springhtmxfullstacktest/SpringHtmxFullstackTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.longpham.springhtmxfullstacktest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringHtmxFullstackTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
